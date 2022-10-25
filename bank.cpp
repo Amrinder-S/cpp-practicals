e319eb509755c60e3e48323933ee4888f3f6680e
Amrr <skymathew1@gmail.com>
2022-10-25 21:00:06 +0530

    Finally complete

--- bank.cpp
+++ bank.cpp
@@ -1,9 +1,8 @@
 #define PENELTY_AMOUNT 500 //Penelty Amount, can be changed from here if required.
-#define WITHDRAW 0 
-#define DEPOSIT 1
-#define APPLY_PENELTY 2
+#define MINIMUM_BALANCE 10000
 #include<iostream>
 #include<string>
+#include<math.h>
 using namespace std;
 
 class account {
@@ -13,31 +12,43 @@ class account {
     public:
     static int account_number_count; //this is to keep track of account number, so no same account with two different account numbers is made.
     void accountCreate(string);
-    void alterBalance(int argument) // this function is made to do withdraw, deposit and applying penelty if required.
-    {
-        int newBal = 0;
-        if(argument==APPLY_PENELTY) //if an argument (2) is supplied, apply penelty
-        {
-            cout<<"\nService charge of "<<PENELTY_AMOUNT<<" applied to account #"<<account_number<<endl;
-            cout<<"Old balance: "<<balance;
-            balance-=PENELTY_AMOUNT;
-            cout<<" | New balance: "<<balance<<endl;
-        }
-        else if (argument==WITHDRAW||argument==DEPOSIT) // if withdraw or deposit argument is passed, do the desired operation.
-        {
 
-        cout<<"\nEnter the amount to "<<(argument?"deposit":"withdraw")<<": ";
-        cin>>newBal;
-        if(newBal<0) newBal=-newBal; //if a negative value is supplied, convert it to positive
-        cout<<"Amount "<<newBal<<(argument?" deposited to":" withdrawn from")<<" bank account #"<<account_number<<endl;
-        cout<<"Old balance: "<<balance;
-        argument? balance+=newBal : balance-=newBal; //Use of conditional operator to alter balance based on argument.
-        cout<<" | New balance: "<<balance<<endl;
-        }
-    }
-    void showBalance()
+    void deposit()
     {
-        cout<<"\nBalance for Account #"<<account_number<<" : "<<balance<<"\nCustomer Name:"<<customer_name<<endl;
+        int newBalance=0;
+        cout<<"\nEnter the amount to deposit: ";
+        cin>>newBalance;
+        if(newBalance<0) newBalance=-newBalance; //if a negative value is supplied, convert it to positive
+        cout<<"Amount "<<newBalance<<" deposited to bank account #"<<account_number<<endl;
+        cout<<"Old balance: "<<balance;
+        balance+=newBalance;    
+        cout<<" | New balance: "<<balance<<endl;
+    }
+
+    void withdraw()
+    {
+        int newBalance=0;
+        cout<<"\nEnter the amount to withdraw: ";
+        cin>>newBalance;
+        if(newBalance<0) newBalance=-newBalance; //if a negative value is supplied, convert it to positive
+        cout<<"Amount "<<newBalance<<" withdrawn from bank account #"<<account_number<<endl;
+        cout<<"Old balance: "<<balance;
+        balance-=newBalance;    
+        cout<<" | New balance: "<<balance<<endl;
+    }
+    void displayBalance()
+    {
+        cout<<"\nBalance for Account #"<<account_number<<" : "<<balance<<"\nCustomer Name: "<<customer_name<<endl;
+    }
+
+    void alterBalance(float newBalance=0) //function to change balance, in case of penelty or compound interest
+    {
+        balance+=newBalance;
+    }
+
+    float returnBalance()
+    {
+        return balance;
     }
 
 };
@@ -46,10 +57,10 @@ int account::account_number_count;
 void account::accountCreate(string account_type)
 {
         {
-        cout<<"\nEnter customer Name:";
+        cout<<"\nEnter customer Name: ";
         getline(cin >> ws, customer_name, '\n');
         account_number = account::account_number_count;
-        cout<<"Account number "<<account::account_number_count<<" Created.\nCustomer Name: "<<customer_name<<"\nBalance: "<<balance<<"\nAccount Type: "<<account_type<<endl;
+        cout<<"Account number "<<account::account_number_count<<" Created.\nCustomer Name: "<<customer_name<<"\nBalance: "<<balance<<"\nAccount Type: "<<account_type<<" account."<<endl;
         account::account_number_count++;
     }
 }
@@ -57,35 +68,54 @@ void account::accountCreate(string account_type)
 class cur_acct: public account {
 public:
 //check book facility coming soon
-void checkPenelty() {alterBalance(APPLY_PENELTY);}
-void withdraw() {alterBalance(WITHDRAW); }
-void deposit() {alterBalance(DEPOSIT);}
-void createAccount() { accountCreate("Current");}
-void showBal() {cout<<balance;}
+cur_acct() { accountCreate("Current");}
+void applyPenelty()
+{
+    if(returnBalance()<MINIMUM_BALANCE)
+        {
+            cout<<"\nService charge of "<<PENELTY_AMOUNT<<" applied.";
+            cout<<"\nOld Balance: "<<returnBalance();
+            alterBalance(-PENELTY_AMOUNT);
+            cout<<" | New Balance: "<<returnBalance()<<endl;
+        }
+}
+void chequeBookFacility()
+{
+    cout<<"\nCheque book facility is available. Please apply at local branch.\n";
+}
+
 };
 
 class sav_acct: public account {
 public:
 //Compound Interest coming soon
-void withdraw() {alterBalance(WITHDRAW); }
-void deposit() {alterBalance(DEPOSIT);}
-void createAccount() { accountCreate("Current");}
+    sav_acct() { accountCreate("Savings");}
+    
+    void compoundInterest()
+    {
+    float principal = returnBalance(),rate=5,time=1,ci;
+
+    ci = principal*pow((1+rate/100),time);
+
+    cout<<"\nCompound interest of "<<ci<<" deposited into your account."<<endl;
+    cout<<"Old balance: "<<returnBalance();
+    alterBalance(ci - returnBalance());
+    cout<<" | New balance: "<<returnBalance()<<endl;
+    }
 };
 
 int main()
 {
-
     cur_acct a;
-    a.createAccount();
-    a.showBal();
-    /*account a,b;
-    a.accountCreate();
-    a.alterBalance(DEPOSIT);
-    a.showBalance();
-    a.alterBalance(APPLY_PENELTY);
-    a.showBalance();
-    a.alterBalance(WITHDRAW);
-    a.showBalance();
-*/
+    a.deposit();
+    a.withdraw();
+    a.displayBalance();
+    a.applyPenelty();
+    a.chequeBookFacility();
 
+    sav_acct b;
+    b.deposit();
+    b.withdraw();
+    b.displayBalance();
+    b.compoundInterest();
 }
